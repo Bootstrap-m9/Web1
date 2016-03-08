230ff628289f3ff8c0c7ed1f0117f3fbfb9fee04
NeoDaark <DaarkAssassin@gmail.com>
2016-03-08 21:21:47 +0100

    aaaa

--- index.php
+++ index.php
@@ -8,7 +8,12 @@
             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
         </div>
         <div class="row">
-
+            <div class="col-md-3">
+                      
+            </div><!--/.col-md-3-->
+            <div class="col-md-9">
+                      
+            </div><!--/.col-md-3-->
         </div><!--/.row-->    
     </div><!--/.container-->
 </section><!--/#feature-->
