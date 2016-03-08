6b8ee41f714665154380ceefc2ddcfe2ec278961
NeoDaark <DaarkAssassin@gmail.com>
2016-03-08 21:14:39 +0100

    The second Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+    include("header.php"); 
+?>
+<section id="feature" >
+    <div class="container">
+       <div class="center wow fadeInDown">
+            <h2>Features</h2>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
+        </div>
+        <div class="row">
+
+        </div><!--/.row-->    
+    </div><!--/.container-->
+</section><!--/#feature-->
+
+<?php 
+    include("footer.php");
+?>
+    
+
